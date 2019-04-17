d035c8e98916ccd5757e4f903064320ad1480280
Sergey Khmelevskoy <xs290@me.com>
2019-04-17 16:56:26 +0300

    create emulator and bios page, coverminheight func

---
+++ src/components/games/_game.js
@@ -0,0 +1,27 @@
+//////////
+// CICKS
+//////////
+(function($, APP) {
+  APP.Components.Game = {
+    init: function() {
+      this.setCoverMinHeight();
+      this.watchResize();
+    },
+    watchResize: function() {
+      _window.on('resize', debounce(this.setCoverMinHeight.bind(this), 200));
+    },
+    setCoverMinHeight: function() {
+      var $cover = $('[js-cover-min-height]');
+      if ($cover.length === 0) return;
+      var $target = $($cover.data('watch'));
+
+      if (window.innerWidth >= 768) {
+        $cover.css({
+          'min-height': $target.outerHeight(),
+        });
+      } else {
+        $cover.attr('style', '');
+      }
+    },
+  };
+})(jQuery, window.APP);
--- src/js/app.js
+++ src/js/app.js
@@ -75,6 +75,7 @@ var easingSwing = [0.02, 0.01, 0.47, 1]; // default jQuery easing
     // All components from `src/componenets`
     app.initComponents = function() {
       APP.Components.Header.init();
+      APP.Components.Game.init();
     };
 
     return app;
